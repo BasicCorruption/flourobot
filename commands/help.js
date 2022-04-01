ddf578a21c3a5093e640e71873ed78929f57fa0b
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-31 21:07:09 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,11 +6,28 @@ module.exports = {
     description:
         "List all available commands or info about a specific command.",
     execute(message, args) {
-        message.channel.send(
-            new Discord.MessageEmbed()
-                .setColor("#0099ff")
-                .setTitle("Help")
-                .setDescription("A list of all available commands.")
-        );
+        // message.channel.send(
+        //     `**Help**\n\n--| Commands |--\n\nHELP - ${
+        //         require("./help").description
+        //     }\nPING - ${require("./ping").description}`
+        // );
+
+        const hembed = new Discord.MessageEmbed({
+            title: "Help",
+            description:
+                "List all available commands or info about a specific command.",
+            color: 0x00ff00,
+            fields: [
+                {
+                    name: "Commands",
+                    value: [
+                        "HELP | " + require("./help").description,
+                        "PING | " + require("./ping").description,
+                    ],
+                },
+            ],
+        });
+
+        message.channel.send({ embeds: [hembed] });
     },
 };
