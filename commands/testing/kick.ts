4fb9d1d2c0fa3cd86fb1c6a3c8f4c3a5f38d2994
BasicCorruption <saytleader@outlook.com>
2022-06-15 22:00:23 -0400

    Update kick.ts

--- commands/testing/kick.ts
+++ commands/testing/kick.ts
@@ -1,3 +1,4 @@
+import { GuildMember } from "discord.js";
 import { ICommand } from "wokcommands";
 
 export default {
@@ -5,4 +6,38 @@ export default {
 	description: "Kick a user from the server.",
 
 	permissions: ["KICK_MEMBERS"],
+
+	slash: 'both',
+	testOnly: true,
+
+	guildOnly: true,
+
+	minArgs: 2,
+	expectedArgs: "<user> <reason>",
+	expectedArgsTypes: ["USER", "STRING"],
+
+	callback: ({ message, interaction, args }) => {
+		const target = message ? message.mentions.members?.first() : interaction.options.getMember('user') as GuildMember;
+		if (!target) {
+			return "Please mention a user to kick.";
+		}
+		if (!target.kickable) {
+			return {
+				custom: true,
+				content: "I can't kick this user.",
+				ephemeral: true,
+			};
+		}
+
+		args.shift();
+		const reason = args.join(' ');
+
+		target.kick(reason);
+
+		return {
+			custom: true,
+			content: `You kicked <@${target.id}>`,
+			ephemeral: true,
+		};
+	}
 } as ICommand
