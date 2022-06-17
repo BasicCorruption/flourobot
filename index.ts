1ab872f09f9fb0efd84f0e77e5c3464b79f1eb62
BasicCorruption <saytleader@outlook.com>
2022-06-16 21:51:52 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -10,6 +10,7 @@ const client = new DiscordJS.Client({
 		Intents.FLAGS.GUILDS,
 		Intents.FLAGS.GUILD_MESSAGES,
 		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_PRESENCES,
 	]
 });
 
@@ -23,8 +24,10 @@ client.on("ready", async () => {
 
 	new WOKCommands(client, {
 		commandsDir: path.join(__dirname, "commands"),
+		featuresDir: path.join(__dirname, "features"),
 		typeScript: true,
 		testServers: '926898971522256937',
+		botOwners: ['581632264085962753'],
 		mongoUri: `mongodb://${process.env.MONGO_URI}/${process.env.MONGO_DB}` || '',
 	});
 });
